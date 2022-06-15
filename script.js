9f4b720900916d69ee300e928f2f354918d8941a
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-15 12:45:38 -0400

    Create a div square via DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('container');
+
+//Create a square div
+
+let div = document.createElement('div');
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.border = "1px solid black";
+div.style.backgroundColor = "lightblue"
+container.appendChild(div);
+
+

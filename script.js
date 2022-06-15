4bf016a1b1e6c7d97213d9cce814c05087517d1a
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-15 12:49:09 -0400

    Create 256 div squares DOM

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.getElementById('container');
 
-//Create a square div
-
-let div = document.createElement('div');
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.border = "1px solid black";
-div.style.backgroundColor = "lightblue"
-container.appendChild(div);
+//Create a 256 square div
+for (let i=1; i <= 256; i++) {
+    let div = document.createElement('div');
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.border = "1px solid black";
+    div.style.backgroundColor = "lightblue";
+    container.appendChild(div);
+}
 
 

a0ba7151fa921588e2173b03b4a56d505b13bcc2
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-15 13:19:54 -0400

    Hover effect that changes square color

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ for (let i=1; i <= 256; i++) {
     div.style.height = "50px";
     div.style.border = "1px solid black";
     div.style.backgroundColor = "lightblue";
+    div.className = "square";
+    //div.textContent = div.className;
     container.appendChild(div);
 }
 
+//Hover effect that changes square color
+const divArray = document.querySelectorAll(".square");
+divArray.forEach((square) => {
+        square.addEventListener("mouseenter", () =>{
+            square.style.backgroundColor = "red"
+        })
+    });
 

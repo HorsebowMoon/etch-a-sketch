e7242e00cbecb3c6a5f4cd6dd329ab3bf1b39e93
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-15 13:31:06 -0400

    Create button

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const container = document.getElementById('container');
+const body = document.getElementById('body');
+
+//create button
+const numberButton = document.createElement('button');
+numberButton.textContent = "click me";
+body.appendChild(numberButton);
+
 
 //Create a 256 square div
 for (let i=1; i <= 256; i++) {

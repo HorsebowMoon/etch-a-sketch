e373788d8a7b84fcb09c5954d3961286131810c9
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-16 14:08:36 -0400

    Add class attribute to lines and squares

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //functions
 function createLine(number){
     let line = document.createElement("div");
-    line.className = "line";
+    line.setAttribute("class", "line");
 
     for (let i = 0; i < number; i++){
         let square = document.createElement('div');
+        square.setAttribute("class", "square");
         line.appendChild(square);
     };
 

19ff7f3de5835e09d5c45e47653e686b52624275
HorsebowMoon <yesitsmeer@gmail.com>
2022-06-16 14:50:14 -0400

    Add button to pick number of squares per side

--- script.js
+++ script.js
@@ -18,18 +18,55 @@ function createGrid(number){
     };
 }
 
+function deleteGrid() {
+    let board = document.getElementById("board");
+    let first = board.firstElementChild;
+    while (first) {
+        first.remove();
+        first = board.firstElementChild;
+    }
+}
+
+function colorGrid(){
+    const squares = document.querySelectorAll("div.square");
+
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        }
+        );
+});
+}
+
 //variables
+
+const buttonRow = document.createElement('div');
+buttonRow.setAttribute("class", "buttonRow");
+document.body.append(buttonRow);
+
+const buttonSquares = document.createElement('button');
+buttonSquares.textContent = "click me!"
+
+const buttonClear = document.createElement('button');
+buttonClear.textContent = "clear board"
+
+buttonRow.appendChild(buttonSquares);
+buttonRow.appendChild(buttonClear);
+
 const board = document.getElementById("board");
+
 
 //code
 
 createGrid(16);
 
-const squares = document.querySelectorAll("div.square");
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    }
-    );
+
+colorGrid();
+
+buttonSquares.addEventListener("click", () => {
+    let sideNumbers = Number(prompt("How many squares per side do you want? "));
+    deleteGrid();
+    createGrid(sideNumbers);
+    colorGrid();
 });
